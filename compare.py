7dce54f0c1394907f42d1aa4df1e225658998aff
James Greene <james.joshua.greene@gmail.com>
2026-04-26 13:24:51 -0400

    Initial programming challenge folder

--- compare.py
+++ compare.py
@@ -3,13 +3,13 @@ import sys
 print(sys.argv[1])
 print(sys.argv[2])
 
-f1 = open(sys.argv[1], encoding="utf-8")
-f2 = open(sys.argv[2], encoding="utf-16")
+f1 = open(sys.argv[1], encoding="utf-16")
+f2 = open(sys.argv[2], encoding="utf-8")
 
 l1 = f1.read().split("\n")
 l2 = f2.read().split("\n")
 
-for i in range(len(l1)):
+for i in range(len(l1)-1):
     if int(l1[i].strip())!=int(l2[i].strip()):
         print("line ", i)
         print("Left = ", l1[i], " Right = ", l2[i])
