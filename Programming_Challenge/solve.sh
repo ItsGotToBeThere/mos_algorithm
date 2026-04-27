493eb2c3aec7bd78e4e1373ca546c80ea751b31c
DragonHuntrX <John@Devost.net>
2026-04-26 23:03:04 -0400

    Fixed test cases and removed empty output

--- Programming_Challenge/solve.sh
+++ Programming_Challenge/solve.sh
@@ -1,1 +1,1 @@
-cat ./tests/test$1.in | cargo run 2>/dev/null > tests/test$1.out
+for i in {21..50}; do echo Solving $i; cat ./tests/test$i.in | cargo run -r 2>/dev/null > tests/test$i.out;  done
--- Programming_Challenge/tests/gen_more_tests.rs
+++ Programming_Challenge/tests/gen_more_tests.rs
@@ -1,9 +1,9 @@
 use std::fs;
 
 fn main() {
-    for x in 25..=50 {
+    for x in 21..=50 {
         let out = std::process::Command::new("python")
-            .args(["./generate_tests.py", "L"])
+            .args(["./generate_tests.py", "l"])
             .output()
             .expect("Should run just fine...")
             .stdout;
